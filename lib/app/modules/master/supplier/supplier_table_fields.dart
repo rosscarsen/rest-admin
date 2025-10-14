04af080191eb7a0d8fc6be307dc78ac20d0d6989
rosscarsen <rosscarsen@gmail.com>
2025-10-14 17:33:14 +0800

    feat(供应商): 添加供应商表字段常量类

---
+++ lib/app/modules/master/supplier/supplier_table_fields.dart
@@ -0,0 +1,20 @@
+class SupplierTableFields {
+  static const String mAddress = "mAddress";
+  static const String mAns_Back = "mAns_Back";
+  static const String mCode = "mCode";
+  static const String mContact = "mContact";
+  static const String mFax_No = "mFax_No";
+  static const String mFullName = "mFullName";
+  static const String mPhone_No = "mPhone_No";
+  static const String mSimpleName = "mSimpleName";
+  static const String mST_Currency = "mST_Currency";
+  static const String mST_Payment_Days = "mST_Payment_Days";
+  static const String mST_Discount = "mST_Discount";
+  static const String mST_Payment_Method = "mST_Payment_Method";
+  static const String mST_Payment_Term = "mST_Payment_Term";
+  static const String mTelex = "mTelex";
+  static const String T_Supplier_ID = "T_Supplier_ID";
+  static const String mEmail = "mEmail";
+  static const String mRemarks = "mRemarks";
+  static const String mNon_Active = "mNon_Active";
+}
