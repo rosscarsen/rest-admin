def39f06e57c0a5067b5079aba12f95a9a5dc27b
rosscarsen <rosscarsen@gmail.com>
2025-11-03 11:24:48 +0800

    feat(支付方式): 实现支付方式管理功能
    - 添加支付方式列表获取、编辑和删除功能
    - 使用DataGrid展示支付方式数据
    - 实现分页加载和权限验证
    - 添加表单验证和错误处理逻辑

--- lib/app/modules/master/pay_method/pay_method_controller.dart
+++ lib/app/modules/master/pay_method/pay_method_controller.dart
@@ -1,15 +1,29 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:get/get.dart';
 
-class PayMethodController extends GetxController {
-  final count = 0.obs;
+import '../../../config.dart';
+import '../../../mixin/loading_state_mixin.dart';
+import '../../../model/pay_method/pay_method_data.dart';
+import '../../../model/pay_method/pay_method_page_model.dart';
+import '../../../routes/app_pages.dart';
+import '../../../service/dio_api_client.dart';
+import '../../../service/dio_api_result.dart';
+import '../../../translations/locale_keys.dart';
+import '../../../utils/custom_alert.dart';
+import '../../../utils/custom_dialog.dart';
+import 'pay_method_data_source.dart';
+
+class PayMethodController extends GetxController with LoadingStateMixin<List<PayMethodData>?> {
+  final GlobalKey<FormBuilderState> formKey = GlobalKey<FormBuilderState>();
+  final ApiClient apiClient = ApiClient();
+  late PayMethodDataSource dataSource;
   @override
   void onInit() {
     super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
+    updateDataGridSource();
   }
 
   @override
@@ -17,5 +31,102 @@ class PayMethodController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
+  /// 重载数据
+  void reloadData() {
+    FocusManager.instance.primaryFocus?.unfocus();
+    totalPages = 0;
+    currentPage = 1;
+    updateDataGridSource();
+  }
+
+  /// 更新数据源
+  void updateDataGridSource() {
+    getList().then((_) {
+      dataSource = PayMethodDataSource(this);
+    });
+  }
+
+  /// 获取列表
+  Future<void> getList() async {
+    isLoading = true;
+    data?.clear();
+    try {
+      formKey.currentState?.saveAndValidate();
+      final param = {
+        'page': currentPage,
+        ...formKey.currentState?.value ?? {},
+        "lang": (Get.locale?.toString() ?? "zh_HK").toLowerCase(),
+      };
+      final DioApiResult dioApiResult = await apiClient.get(Config.paymentMethod, data: param);
+
+      if (!dioApiResult.success) {
+        if (!dioApiResult.hasPermission) {
+          hasPermission = false;
+        }
+        CustomDialog.errorMessages(dioApiResult.error ?? LocaleKeys.unknownError.tr);
+        return;
+      }
+      if (dioApiResult.data == null) {
+        CustomDialog.errorMessages(dioApiResult.error ?? LocaleKeys.unknownError.tr);
+        return;
+      }
+      hasPermission = true;
+      //logger.f(dioApiResult.data);
+      final resultModel = payMethodPageModelFromJson(dioApiResult.data.toString());
+      if (resultModel.status == 200) {
+        data = resultModel.apiResult?.data ?? [];
+        totalPages = (resultModel.apiResult?.lastPage ?? 0);
+        totalRecords = (resultModel.apiResult?.total ?? 0);
+      } else {
+        CustomDialog.errorMessages(LocaleKeys.getDataException.tr);
+      }
+    } finally {
+      isLoading = false;
+    }
+  }
+
+  /// 编辑
+  void edit({String? id}) async {
+    Get.toNamed(Routes.PAY_METHOD_EDIT, parameters: id == null ? null : {'id': id.toString()});
+  }
+
+  /// 删除单行数据
+  void deleteRow(String? id) async {
+    CustomAlert.iosAlert(
+      showCancel: true,
+      message: LocaleKeys.deleteConfirmMsg.tr,
+      onConfirm: () async {
+        try {
+          CustomDialog.showLoading(LocaleKeys.deleting.tr);
+          final DioApiResult dioApiResult = await apiClient.get(Config.paymentMethodDelete, data: {"id": id});
+
+          if (!dioApiResult.success) {
+            CustomDialog.errorMessages(dioApiResult.error ?? LocaleKeys.unknownError.tr);
+            return;
+          }
+          if (dioApiResult.data == null) {
+            CustomDialog.errorMessages(dioApiResult.error ?? LocaleKeys.unknownError.tr);
+            return;
+          }
+          final Map<String, dynamic> apiData = jsonDecode(dioApiResult.data!) as Map<String, dynamic>;
+          switch (apiData['status']) {
+            case 200:
+              CustomDialog.successMessages(LocaleKeys.deleteSuccess.tr);
+              data?.removeWhere((element) => element.tPayTypeId == id);
+              dataSource.updateDataSource();
+              break;
+            case 201:
+              CustomDialog.errorMessages(LocaleKeys.deleteFailed.tr);
+              break;
+            default:
+              CustomDialog.errorMessages(LocaleKeys.unknownError.tr);
+          }
+        } catch (e) {
+          CustomDialog.errorMessages(LocaleKeys.deleteFailed.tr);
+        } finally {
+          CustomDialog.dismissDialog();
+        }
+      },
+    );
+  }
 }
