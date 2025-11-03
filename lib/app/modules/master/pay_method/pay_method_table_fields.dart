16b60276a1cb5ed0a49c04c3311a6bc6aa823e5d
rosscarsen <rosscarsen@gmail.com>
2025-11-03 11:25:20 +0800

    feat(支付方式): 添加支付方式表字段常量定义

---
+++ lib/app/modules/master/pay_method/pay_method_table_fields.dart
@@ -0,0 +1,12 @@
+class PayMethodTableFields {
+  static const String mPayType = "mPayType";
+  static const String tPayTypeID = "T_PayType_ID";
+  static const String mSort = "mSort";
+  static const String mPrePaid = "mPrePaid";
+  static const String mCreditCart = "mCreditCart";
+  static const String mCardType = "mCardType";
+  static const String mCom = "mCom";
+  static const String mNoDrawer = "mNoDrawer";
+  static const String tPayTypeOnline = "t_paytype_online";
+  static const String mHide = "mHide";
+}
